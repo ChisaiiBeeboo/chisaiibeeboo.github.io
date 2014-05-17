0f22832eccc863d194f1ad00fba09a2591ab7862
Kate's Macbook Air <macbookair@Kate-MacBook-Air.local>
2014-05-16 19:18:00 -0700

    test page

--- unit1_projects/test.rb
+++ unit1_projects/test.rb
@@ -1,15 +1,12 @@
 ############## => PAD ARRAY #################
 # class Array
-	# def pad(total, var=nil)
-	# 	(self << total.times.map{ var }).flatten
-	# end
-
-# 	def pad!(total, var=nil)
-# 		self.clone.pad
+# 	def pad(min_size, opt=nil)
+# 		total = min_size - self.length
+# 		(self << total.times.map{ opt ).flatten
 # 	end
 # end
 
-# p [1,2,3].pad(5, 'apple')
+# p [1,2,3].pad(0)
 
 # p pad([1,2,3], 5)
 
@@ -19,12 +16,12 @@
 
 # p (arr << 2.times.map{ 'apple' }).flatten
 
-#########################################################
+###################### GROUP PROJECT ###################################
 
 
-i_want_pets = ["I", "want", 3, "pets", "but", "I", "only", "have", 2, ":(." ]
-my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, 
-  "Annabelle" => 0, "Ditto" => 3}
+# i_want_pets = ["I", "want", 3, "pets", "but", "I", "only", "have", 2, ":(." ]
+# my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, 
+#   "Annabelle" => 0, "Ditto" => 3}
 
 # def my_array_modification_method(i_want_pets, 1) 
 # end
@@ -37,18 +34,14 @@ my_family_pets_ages = {"Evi" => 6, "Hoobie" => 3, "George" => 12, "Bogart" => 4,
 
 # p i_want_pets.grep(\d)
 
-i_want_pets.each {|x| x.class == Fixnum {x + 1} }
+# i_want_pets.each {|x| x.class == Fixnum x + 1 }
+
+################# CIPER ##################################
 
 
 
 
+str = "i heart 300 kazoozles"
 
-
-
-
-
-
-
-
-
+p str.match(/\d+/)
 
