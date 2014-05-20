f8fc119ce6d93bfaa6a7840467d8fe10d407abbb
Kate's Macbook Air <macbookair@Kate-MacBook-Air.local>
2014-05-19 20:44:05 -0700

    Updated blog

--- unit1_projects/test.rb
+++ unit1_projects/test.rb
@@ -107,8 +107,27 @@
 
 
 
+# class Die
+#   def initialize(sides)
+#  	raise ArgumentError, 'Wrong number' unless sides > 0
+#   	@sides = sides
 
-//p ('a'..'z').to_a
+#   end
+  
+#   def sides
+# 	@sides
+#   end
+  
+#   def roll
+#     rand(1..6)
+#   end
+# end
+
+
+# die = Die.new(3)
+# p die.sides # returns 3
+# p die.roll # returns a random number between 1 and 6
+# p die = Die.new(0) # Should throw ArgumentError 'Wrong number'
 
 
 
@@ -116,7 +135,84 @@
 
 
 
-class ARRAY
+
+# class Die
+#   def initialize(labels)
+#   	raise ArgumentError, 'No Empty Arrays' if labels[0] == nil
+#   	@sides = labels.length
+#   	@labels = labels
+#   end
+
+#   def sides
+#   	@sides
+#   end
+
+#   def roll
+#   	@labels.sample
+#   end
+# end
+
+
+# die = Die.new(['A', 'B', 'C', 'D', 'E', 'F'])
+# p die.sides # still returns the number of sides, in this case 6
+# p die.roll # returns one of ['A', 'B', 'C', 'D', 'E', 'F'], randomly
+# p die = Die.new([]) # should throw "No Empty Arrays" ArgumentError
+
+class GuessingGame
+	def initialize(answer)
+		@answer = answer
+	end
+
+	def guess(guess)
+		if guess > answer
+			:higher
+		elsif guess < answer
+			:lower
+		else
+			:correct
+		end
+	end
+
+	def solved?
+		if guess == :correct ? true : false
+	end
+
+end
+
+
+game = GuessingGame.new(10)
+
+game.solved?   # => false
+
+game.guess(5)  # => :low
+game.guess(20) # => :high
+game.solved?   # => false
+
+game.guess(10) # => :correct
+game.solved?   # => true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ unit1_projects/untitled.rb
@@ -0,0 +1,35 @@
+# for arr multiply every other element by 2 up until element 6
+
+p num = 4563960122001999
+p arr = num.to_s.split('')
+p arr = arr.rotate(-2)
+dbl_arr = []
+p arr.each do |x|  
+	until arr.length
+		x * 
+end
+
+# p (0..arr.length).step(2).each do |index|
+#   value_you_care_about = arr[index]
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
